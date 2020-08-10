034668ea5f210a6e2e4ea2ece25d4ecb14638ed8
bfield1 <bernard.field1@monash.edu>
2020-08-10 15:26:42 +1000

    Initial commit. Copied over files from non-git workspace. Established basic structure.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is code for running simulations of a Mean Field Hubbard model.
---
+++ hubbard/kagome_hubbard_utils.py
@@ -0,0 +1,236 @@
+#!/usr/bin/python3
+
+"""
+Helper scripts for analysing my Mean field Hubbard model of the Kagome
+lattice.
+
+Created: 2020-08-04
+Last Modified: 2020-08-07
+Author: Bernard Field
+"""
+
+from kagome_hubbard import KagomeHubbard, ConvergenceWarning, MixingError
+from kagome_hubbard import choose_alpha
+import warnings
+import os.path
+import progress
+import numpy as np
+from time import sleep
+from glob import glob
+
+def converge(kagome,rdiff,rdiff_initial=1e-2,T=None,debug=False,interval=500):
+    """
+    Brings a KagomeHubbard model to self-consistency,
+    regardless of how long it takes.
+    Inputs: kagome - KagomeHubbard object.
+        rdiff - number, target residual
+        rdiff_initial - number, target residual for the initial linear mixing step.
+        T - number, optional. Temperature for Fermi Dirac distribution.
+        debug - Boolean, whether to print progress.
+        interval - integer. How often to print progress.
+    Last Modified: 2020-08-07
+    """
+    # Suppress ConvergenceWarnings.
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",ConvergenceWarning)
+        # We need to perform an initial linear mixing.
+        while kagome.residual(T) > rdiff_initial:
+            last_residual = kagome.residual(T)
+            kagome.linear_mixing(max_iter=interval,ediff=0.1,
+                                 rdiff=rdiff_initial,T=T,print_residual=False)
+            if debug: print("Linear mixing. Residual="+str(kagome.residual(T)))
+            # Catch a pathological case where residual doesn't decrease.
+            if np.isclose(last_residual,kagome.residual(T)):
+                if debug: print("Residual didn't decrease. Trying smaller mixing.")
+                kagome.linear_mixing(max_iter=interval,ediff=0.1,mix=0.1,
+                                 rdiff=rdiff_initial,T=T,print_residual=False)
+                if debug: print("Linear mixing (mix=0.1). Residual="
+                                +str(kagome.residual(T)))
+        # Now do main mixing.
+        while kagome.residual(T) > rdiff:
+            try:
+                kagome.pulay_mixing(max_iter=interval,rdiff=rdiff,T=T)
+                if debug: print("Pulay mixing. Residual="+str(kagome.residual(T)))
+            except MixingError:
+                # Pulay mixing has a chance to take the electron density out of
+                # bounds. I've never seen it so far, but I'm ready to catch it.
+                if debug: print("Failure in Pulay mixing. Residual="
+                                +str(kagome.residual(T))+". Doing some linear mixing.")
+                kagome.linear_mixing(max_iter=10,rdiff=rdiff,ediff=0.1,T=T)
+                if debug: print("Linear mixing. Residual="+str(kagome.residual(T)))
+
+def sweep_spin(kagome,n,nsteps,rdiff,rdiff_initial=1e-2,
+               T=None,interval=500,positive_only=True,repeats=1,verbose=False,
+               mmin=None,mmax=None):
+    """
+    For a given set of parameters, steps through different nup and ndown ratios.
+    Relaxes the Kagome systems, and returns all the results as a list.
+    
+    Inputs: kagome - template KagomeHubbard object (with right size,
+            U, kinetic energy, etc., but electrons are ignored)
+        n - number of electrons.
+        nsteps - how many steps (as in linspace).
+        rdiff, rdiff_initial, T, interval - as for converge
+        positive_only - Boolean. Only check positive magnetization.
+            Recommend True when magnetic field is zero, becasue symmetry.
+            Ignored if mmin is set.
+        repeats - positive integer. How many times to repeat each measurement.
+        verbose - Boolean.
+        mmin - optional number. Minimum magnetization to look at.
+            Defaults to 0 if positive_only else -mmax.
+        mmax - optional number. Maximum magnetization to look at.
+            Defaults to the smaller of n or nsites.
+    
+    Output: list of KagomeHubbard objects.
+
+    Last Modified: 2020-08-07
+    """
+    # Initialise
+    kagome_list = [] # Results list
+    nsites = kagome.nrows*kagome.ncols*3
+    # Bounds
+    if mmax is None:
+        mmax = min(n,nsites)
+    if mmin is None:
+        if positive_only:
+            mmin = 0
+        else:
+            mmin = -mmax
+    nupmin = max(0,n-nsites,(n+mmin)/2)
+    nupmax = min((mmax+n)/2,n,nsites)
+    # Pick an alpha
+    alpha = choose_alpha(nsites,min(nupmax,nsites/2))*10
+    # Progress bar
+    if not verbose:
+        progress.new(60,nupmax,nupmin)
+    else:
+        print("Nup from "+str(nupmin)+" to "+str(nupmax))
+    # Iterate
+    samples,stepsize = np.linspace(nupmin,nupmax,nsteps,retstep=True)
+    try:
+        for nup in samples:
+            for i in range(repeats):
+                try:
+                    if verbose: print("Nup="+str(nup)+", repeat "+str(i+1))
+                    # Get a copy I can work with.
+                    kag = kagome.copy()
+                    # Set the electron density
+                    kag.set_electrons(nup=nup,ndown=n-nup,method='random',alpha=alpha)
+                    # Converge
+                    converge(kag,rdiff,rdiff_initial,T=T,interval=interval,debug=verbose)
+                    # Record
+                    kagome_list.append(kag)
+                except KeyboardInterrupt:
+                    # If interrupt, give a half second to wait for a second interrupt.
+                    if verbose: print("Skipping...")
+                    sleep(0.5)
+                if not verbose: progress.update(nup+i/repeats*stepsize)
+    except KeyboardInterrupt:
+        # If we interrupt, still return cleanly.
+        if not verbose:
+            print('X',end='')
+        else:
+            print("Aborting.")
+    if not verbose:
+        progress.end()
+    else:
+        print("Finished.")
+    # Return
+    return kagome_list
+
+
+def write(kagome,directory='.',ext=''):
+    """
+    Write kagome to a file in directory.
+    Automatically handles naming conventions.
+    Filename includes U, supercell, and number of electrons.
+    Other details should be specified in ext.
+    Inputs: kagome - KagomeHubbard object.
+        directory - string to a directory.
+        ext - string. Extension to append to automatically
+            generated name (but before the number)
+    Last Modified: 2020-08-04
+    """
+    # Check existence of directory.
+    if not os.path.isdir(directory):
+        raise FileNotFoundError(str(directory)+" not found.")
+    # Create the base name
+    base_name = ('U'+str(kagome.u)+'cell'+str(kagome.nrows)+'x'+str(kagome.ncols)
+            +'n'+str(int(round(kagome.get_electron_number())))+ext)
+    # Append number.
+    i = 0
+    true_name = base_name + '_' + str(i).zfill(4)
+    # Check existence.
+    while os.path.isfile(directory+'/'+true_name):
+        # If exists, try the next number.
+        i += 1
+        true_name = base_name + '_' + str(i).zfill(4)
+    # Write
+    kagome.save(directory+'/'+true_name)
+
+def write_list(kagome_list,directory='.',ext=''):
+    """
+    Iterates over kagome_list and applies write() to each element.
+    Last Modified: 2020-08-06
+    """
+    for kagome in kagome_list:
+        write(kagome,directory,ext)
+
+def read_list(directory='.',pattern='U*cell*x*n*'):
+    """
+    Reads a directory filled with KagomeHubbard files.
+    Uses glob matching for the pattern.
+
+    Inputs: directory - string to a directory
+        pattern - string, glob pattern for the files to load.
+    Output: list of KagomeHubbard objects.
+    Last Modified: 2020-08-06
+    """
+    true_pattern = directory + '/' + pattern
+    files = glob(true_pattern)
+    kag_list = [ KagomeHubbard.load(f) for f in files ]
+    return kag_list
+
+def boltzmann_average(kag_list,T,func,Ten=None):
+    """
+    Takes the mean of some value of KagomeHubbard objects,
+    weighted by a Boltzmann factor.
+    Can also take the minimum energy value with T=0.
+
+    Inputs: kag_list - list of KagomeHubbard objects
+        T - number, temperature for the Boltzmann factor.
+        func - callable. Takes a KagomeHubbard object and returns a number.
+        Ten - number, optional. Temperature for calculating the energy.
+    Output: a number
+    Last Modified: 2020-08-06
+    """
+    # Energies
+    en = [k.energy(Ten) for k in kag_list]
+    if T == 0:
+        # Filter for minimum energy states.
+        boltz = np.zeros_like(en)
+        boltz[en == min(en)] = 1
+        boltz /= boltz.sum() # Normalise
+    else:
+        # Weight states by a Boltzmann factor.
+        boltz = np.exp(-(np.asarray(en)-min(en))/T)
+        # Subtract minimum energy to avoid overflow.
+        # Does not change the outcome.
+        boltz /= boltz.sum() # Normalise
+    # Evaluate the function
+    vals = np.array([ func(k) for k in kag_list ])
+    # Return, taking the weighted average.
+    return np.sum(vals*boltz)
+
+def cull_list(kag_list,e,T=None):
+    """
+    Returns a kag_list with only states with energy less than e above
+    the minimum energy.
+    Inputs: kag_list - list of KagomeHubbard objects
+        e - number
+        T - number, optional, temperature for calculating energy.
+    Output: list of KagomeHubbard objects.
+    Last Modified: 2020-08-06
+    """
+    en = np.array([k.energy(T) for k in kag_list])
+    return list(np.asarray(kag_list)[en <= (en.min()+e)])
